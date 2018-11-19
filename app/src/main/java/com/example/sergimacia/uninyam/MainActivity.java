c04475fde4a21796c3ac22744bddd0b3e3f21a1d
sergi.macia <sergimacinformatica@gmail.com>
2018-11-19 13:36:46 +0100

    vd.0 - Interfície BurgerActivity

--- app/src/main/java/com/example/sergimacia/uninyam/MainActivity.java
+++ app/src/main/java/com/example/sergimacia/uninyam/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.sergimacia.uninyam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_burger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_burger=findViewById(R.id.btn_burger);
+
+        btn_burger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BurgerActivity.class);
+            }
+        });
     }
 }
